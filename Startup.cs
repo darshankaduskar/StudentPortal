a1fadbfe9e871730ca58d40f70f9f014e1ee77e0
Darshan <darshankaduskar169@gmail.com>
2025-02-05 23:38:36 +0530

    updated db 2nd

--- Startup.cs
+++ Startup.cs
@@ -58,6 +58,7 @@ namespace StudentAdminPortal.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Middleweare
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
